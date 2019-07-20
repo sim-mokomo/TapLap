ff35741b77e776246e7f664988f80c7e944070e9
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-21 00:23:11 +0900

    [add] ディレクトリが保持する音声ファイル名を元に、キーを打った時にその音声ファイルを再生できるようにした。

--- src/index.js
+++ src/index.js
@@ -4,70 +4,10 @@ const fileFields = document.querySelector("#fileFields")
 const ioHook = require("iohook")
 const fs = require("fs")
 const path = require("path")
+'use strict'
+const soundPlayer = require("play-sound")()
 var audioSourceList = []
 
-const increaseField = document.querySelector("#increaseFieldBtn")
-increaseField.addEventListener("click", () => {
-
-    var container = document.createElement("div")
-    container.classList.add("container")
-    container.classList.add("file-field")
-    var row = document.createElement("div")
-    row.className = "row"
-    container.appendChild(row)
-
-    var fileField = document.createElement("div")
-    fileField.classList.add("col")
-    fileField.classList.add("s10")
-    fileField.classList.add("input-field")
-
-    var btn = document.createElement("div")
-    btn.classList.add("btn")
-    btn.classList.add("blue")
-    btn.appendChild(document.createElement("span").appendChild(document.createTextNode("File")))
-
-    var input = document.createElement("input")
-    input.className = "file-input"
-    input.type = "file"
-    input.addEventListener("change",(e)=>{
-        audioSourceList = refreshAudioSourceList()
-    })
-    btn.appendChild(input)
-    fileField.appendChild(btn)
-
-    var filePath = document.createElement("div")
-    filePath.className = "file-path-wrapper"
-    var filePathInput = document.createElement("input")
-    filePathInput.classList.add("file-path")
-    filePathInput.classList.add("validate")
-    filePathInput.type = "text"
-
-    var arrowUpButton = document.createElement("button")
-    arrowUpButton.classList.add("btn-small")
-    var arrowUpIcon = document.createElement("i")
-    arrowUpIcon.classList.add("col")
-    arrowUpIcon.classList.add("s1")
-    arrowUpIcon.classList.add("material-icons")
-    arrowUpIcon.appendChild(document.createTextNode("arrow_drop_up"))
-    arrowUpButton.appendChild(arrowUpIcon)
-    arrowUpButton.addEventListener("click",(e) => {
-        var file = input.files[0]
-        shiftUpAudioSourceList(file)
-        audioSourceList = refreshAudioSourceList()
-    })
-    
-    var arrowDownButton = document.createElement("button")
-    arrowDownButton.classList.add("btn-small")
-    var arrowDownIcon = document.createElement("i")
-    arrowDownIcon.classList.add("col")
-    arrowDownIcon.classList.add("s1")
-    arrowDownIcon.classList.add("material-icons")
-    arrowDownIcon.appendChild(document.createTextNode("arrow_drop_down"))
-    arrowDownButton.appendChild(arrowDownIcon)
-    arrowDownButton.addEventListener("click",(e)=>{
-        var file = input.files[0]
-        shiftDownAudioSourceList(file)
-        audioSourceList = refreshAudioSourceList()
 var basePath = ""
 var uploadFilePathNames = []
 const soundUploader = document.getElementById("sound-uploader")
@@ -79,23 +19,6 @@ soundUploader.addEventListener("change", (event) => {
             uploadFilePathNames.push(file)
         })
     })
-
-    filePath.appendChild(filePathInput)
-    fileField.appendChild(filePath)
-
-    row.appendChild(fileField)
-    row.appendChild(arrowUpButton)
-    row.appendChild(arrowDownButton)
-
-    fileFields.appendChild(container)
-})
-const decreaseField = document.querySelector("#decreaseFieldBtn")
-decreaseField.addEventListener("click", () => {
-    if (fileFields.hasChildNodes() == false) {
-        return
-    }
-    var last = fileFields.lastChild
-    fileFields.removeChild(last)
 })
 
 const sequenceSelect = document.getElementById("sequence-select")
@@ -114,34 +37,22 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 ioHook.addListener("keydown", (keyEvent) => {
-    console.log(keyEvent.key)
-    var fileFields = document.querySelector("#fileFields")
-    if (!fileFields.hasChildNodes()) {
+    if(uploadFilePathNames.length == 0){
         return
     }
 
     if (currentSequence == 1) {
-        console.log(audioSequenceIndex)
-        var sequenceFiled = fileFields.children[audioSequenceIndex]
-        var file = sequenceFiled.getElementsByTagName("input")[0].files[0]
-        if (file != null) {
-            var audio = new Audio(URL.createObjectURL(file))
-            audio.play()
-            audio = null
-        }
-        audioSequenceIndex = (audioSequenceIndex + 1) % fileFields.children.length
+        var fileName = path.join(basePath, uploadFilePathNames[audioSequenceIndex])
+        soundPlayer.play(fileName)
+        console.log(fileName)
+        audioSequenceIndex = (audioSequenceIndex + 1) % uploadFilePathNames.length
     }
 
     if (currentSequence == 2) {
-        var randomIndex = getRandom(0, fileFields.children.length - 1)
-        console.log(randomIndex)
-        var randomFileField = fileFields.children[randomIndex]
-        var file = randomFileField.getElementsByTagName("input")[0].files[0]
-        if (file != null) {
-            var audio = new Audio(URL.createObjectURL(file))
-            audio.play()
-            audio = null
-        }
+        var randomIndex = getRandom(0, uploadFilePathNames.length - 1)
+        var fileName = path.join(basePath,uploadFilePathNames[randomIndex])
+        console.log(fileName)
+        soundPlayer.play(fileName)
     }
 })
 ioHook.start()
